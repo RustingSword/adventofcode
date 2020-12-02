868842325ff541193059ee065d2c6c1b72c07449
RustingSword <i@toonaive.me>
2020-12-02 20:12:12 +0800

    day 2

---
+++ 2020/02/sol1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def valid_password_num(data):
+    def is_valid(line):
+        rule, char, password = line.replace(":", "").split()
+        min_cnt, max_cnt = map(int, rule.split("-"))
+        return min_cnt <= password.count(char) <= max_cnt
+    print(sum(is_valid(line) for line in data))
+
+if __name__ == "__main__":
+    data = open("input").read().splitlines()
+    valid_password_num(data)
